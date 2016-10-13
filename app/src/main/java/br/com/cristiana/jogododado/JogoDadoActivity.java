f5aefa7b61b0e467ac1b79c8e074366f4f1c4c10
Cristiana <cristiana.pazos@gmail.com>
2016-10-12 21:21:29 -0300

    Initial commit

---
+++ app/src/main/java/br/com/cristiana/jogododado/JogoDadoActivity.java
@@ -0,0 +1,13 @@
+package br.com.cristiana.jogododado;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JogoDadoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jogo_dado);
+    }
+}
