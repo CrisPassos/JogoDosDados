540aee33c2e5850228cd51969056166389ceefcb
Cristiana <cristiana.pazos@gmail.com>
2016-10-23 13:34:43 -0200

    Resultado falado

--- app/src/main/java/br/com/cristiana/jogododado/JogoDadoActivity.java
+++ app/src/main/java/br/com/cristiana/jogododado/JogoDadoActivity.java
@@ -1,17 +1,22 @@
 package br.com.cristiana.jogododado;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import android.speech.tts.TextToSpeech;
+import android.speech.tts.TextToSpeech.OnInitListener;
+
 import java.util.Random;
 
-public class JogoDadoActivity extends AppCompatActivity {
+public class JogoDadoActivity extends AppCompatActivity implements OnInitListener {
 
     ImageView imgJogador;
     ImageView imgComputador;
+    private TextToSpeech tts;
 
     int imagem[] = {R.drawable.dado1, R.drawable.dado2, R.drawable.dado3, R.drawable.dado4, R.drawable.dado5, R.drawable.dado6};
 
@@ -22,6 +27,11 @@ public class JogoDadoActivity extends AppCompatActivity {
 
         imgJogador = (ImageView) findViewById(R.id.imgJog);
         imgComputador = (ImageView) findViewById(R.id.imgComp);
+
+        //sintetizador de voz
+        Context context = this;
+        OnInitListener listener = this;
+        tts = new TextToSpeech(context, listener);
     }
 
     public void jogar(View v)
@@ -39,14 +49,26 @@ public class JogoDadoActivity extends AppCompatActivity {
 
         if (rJogador > rComputador){
             resultado = "O Jogador Ganhou";
+
         } else if (rComputador > rJogador){
             resultado = "O Computador Ganhou";
         }
 
+        tts.speak(resultado, TextToSpeech.QUEUE_FLUSH, null);
         Toast alert = Toast.makeText(this, resultado, Toast.LENGTH_SHORT);
         alert.show();
 
     }
 
 
+    @Override
+    public void onInit(int status) {
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        tts.shutdown();
+    }
 }
